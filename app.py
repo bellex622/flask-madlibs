853e4e3d39f888fde7c00f5d35f846a6d9bd9bd4
Belle Xu <bellex22@protonmail.com>
2023-07-19 19:13:57 -0400

    finish question form

--- app.py
+++ app.py
@@ -7,3 +7,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get('/questions')
+def loading_questions():
+    """display the form to fill in"""
+    words = silly_story.prompts 
+
+    return render_template("questions.html",words = words)
+
+

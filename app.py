b2a84b267f0accc6b6c7b93f3fbd9a23505e2cd1
michael frederickson <mpfplus@gmail.com>
2023-07-19 18:04:03 -0600

    Reached first code review checkpoint

--- app.py
+++ app.py
@@ -9,10 +9,22 @@ app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
 @app.get('/questions')
-def loading_questions():
+def show_prompts():
     """display the form to fill in"""
-    words = silly_story.prompts 
+    words = silly_story.prompts
 
     return render_template("questions.html",words = words)
+
+
+@app.get('/results')
+def display_story():
+    """display the form to fill in"""
+    answers = request.args.to_dict()
+
+    print("ANSWERS >>>>", answers)
+
+    story = silly_story.get_result_text(answers)
+
+    return render_template("results.html", story = story)
 
 
